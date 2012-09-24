190c838081f7a9081a627f43de7a181caf42bb8b
Dariusz Pasciak <dariusz@8thlight.com>
2012-09-24 14:00:05 -0500

    Error handling.

--- spec/javascripts/AssetRefresherSpec.js
+++ spec/javascripts/AssetRefresherSpec.js
@@ -2,17 +2,15 @@ describe( "AssetRefresher", function() {
   var divId = 'someDiv';
   var refreshRoute = '/some-route';
   var responseText = 'some response';
+  var errorMessage = 'error message';
+
+  var refresher;
 
   beforeEach( function() {
     $('body').append('<div id="' + divId + '"></div>');
     this.server = sinon.fakeServer.create();
 
-    this.server.respondWith( 'GET', refreshRoute,
-              [200, {"Content-Type": 'text/html' }, responseText]);
-
-    var refresher = new AssetRefresher( divId, refreshRoute );
-    refresher.go();
-    this.server.respond();
+    refresher = new AssetRefresher( divId, refreshRoute, errorMessage );
   });
 
   afterEach( function() {
@@ -21,7 +19,21 @@ describe( "AssetRefresher", function() {
   });
 
   it( 'Populates div with AJAX response', function() {
+    this.server.respondWith( 'GET', refreshRoute,
+              [200, {"Content-Type": 'text/html' }, responseText]);
+
+    refresher.go();
+    this.server.respond();
     expect( $( '#' + divId )).toHaveHtml( responseText );
+  });
+
+  it( 'Populates div with error message', function() {
+    this.server.respondWith( 'GET', refreshRoute,
+              [500, {"Content-Type": 'text/html' }, responseText]);
+
+    refresher.go();
+    this.server.respond();
+    expect( $( '#' + divId )).toHaveHtml( errorMessage );
   });
     //jasmine.Clock.useMock();
     //jasmine.Clock.tick( 3000 );
--- web/javascripts/AssetRefresher.js
+++ web/javascripts/AssetRefresher.js
@@ -1,18 +1,20 @@
-function AssetRefresher( divToPopulate, refreshRoute ) {
+function AssetRefresher( divToPopulate, refreshRoute, errorMessage ) {
   this.divToPopulate = divToPopulate;
   this.refreshRoute = refreshRoute;
+  this.errorMessage = errorMessage;
 }
 
 AssetRefresher.prototype.go = function( ) {
+  var div = $("#" + this.divToPopulate);
   var that = this;
 
   $.ajax({
     url: that.refreshRoute,
     success: function( html ){
-      var div = $("#" + that.divToPopulate);
       div.html( html );
     },
     error: function() {
+      div.html( that.errorMessage );
     }
   });
 };
