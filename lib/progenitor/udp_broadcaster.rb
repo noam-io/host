e3dc9d1c4e8091f69bd98156d2a2ed3a44633749
Dariusz Pasciak <dariusz@8thlight.com>
2012-09-25 15:41:21 -0500

    Broadcasting UDP location over all available interfaces.

--- lib/progenitor/udp_broadcaster.rb
+++ lib/progenitor/udp_broadcaster.rb
@@ -4,23 +4,29 @@ module Progenitor
   class UdpBroadcaster
     def initialize(port)
       @port = port
-      @address = '<broadcast>'
-      @message_to_broadcast = "[Maestro@#{local_ip}:#{@port}]"
-
       @socket = UDPSocket.new
       @socket.setsockopt(Socket::SOL_SOCKET, Socket::SO_BROADCAST, true)
     end
 
     def go
-      @socket.send(@message_to_broadcast, 0, @address, @port)
-    rescue Exception
+      network_interfaces.each do |network|
+        ip = network.ip_address
+        broadcast_ip = to_broadcast( ip )
+        @message_to_broadcast = "[Maestro@#{ip}:#{@port}]"
+        @socket.send(@message_to_broadcast, 0, broadcast_ip, @port)
+      end
+    rescue Exception => e
+      p "Error broadcasting Maestro's location over UDP."
     end
 
     private
 
-    def local_ip
-      ip = Socket.ip_address_list.detect{|intf| intf.ipv4_private?}
-      ip.ip_address if ip
+    def network_interfaces
+      Socket.ip_address_list.select{|interface| interface.ipv4_private?}
+    end
+
+    def to_broadcast( ip )
+      ip.gsub(/^(\d+\.\d+\.\d+\.)\d+$/, '\1255')
     end
   end
 end
--- spec/progenitor/udp_broadcaster_spec.rb
+++ spec/progenitor/udp_broadcaster_spec.rb
@@ -2,39 +2,33 @@ require 'progenitor/udp_broadcaster'
 
 describe Progenitor::UdpBroadcaster do
   let(:port) { 24039 }
-  let(:expected_message) { "[Maestro@#{local_ip}:#{port}]" }
-  let(:broadcaster) { described_class.new(port) }
-  let(:data) {[]}
-  let(:address) { '0.0.0.0' }
-  let(:socket) { UDPSocket.new }
+  let(:ip_1_host) { '3.4.5.6' }
+  let(:ip_1_broadcast) { '3.4.5.255' }
+  let(:ip_2_host) { '101.202.303.404' }
+  let(:ip_2_broadcast) { '101.202.303.255' }
+  let(:interface_1) { mock( ipv4_private?: true, ip_address: ip_1_host )}
+  let(:interface_2) { mock( ipv4_private?: true, ip_address: ip_2_host )}
 
-  before :all do
-    BasicSocket.do_not_reverse_lookup = true
-  end
-
+  let(:broadcaster) { described_class.new( port ) }
 
   before :each do
-    socket.bind(address, port)
-  end
-
-  after :each do
-    socket.close
+    network_interfaces = [ interface_1, interface_2 ]
+    Socket.stub(:ip_address_list).and_return( network_interfaces )
   end
 
   it "broadcasts it's IP and port" do
-    broadcast_and_read
-
-    data.count.should == 1
-    data[0].should == expected_message
+    set_expectation( ip_1_host, ip_1_broadcast )
+    set_expectation( ip_2_host, ip_2_broadcast )
+    broadcaster.go
   end
 
   it "can broadcast multiple times" do
-    broadcast_and_read
-    broadcast_and_read
-
-    data.count.should == 2
-    data[0].should == expected_message
-    data[1].should == expected_message
+    set_expectation( ip_1_host, ip_1_broadcast )
+    set_expectation( ip_1_host, ip_1_broadcast )
+    set_expectation( ip_2_host, ip_2_broadcast )
+    set_expectation( ip_2_host, ip_2_broadcast )
+    broadcaster.go
+    broadcaster.go
   end
 
   it 'handles network errors' do
@@ -42,19 +36,11 @@ describe Progenitor::UdpBroadcaster do
     broadcaster.go
   end
 
-  def broadcast_and_read
-    readThread = Thread.new do
-      message, addr = socket.recvfrom(1024)
-      data << message
-    end
-    Thread.pass
-    broadcaster.go
-    readThread.join
-  end
-
-  def local_ip
-    ip = Socket.ip_address_list.detect{|intf| intf.ipv4_private?}
-    ip.ip_address if ip
+  def set_expectation( host_ip, broadcast_ip )
+    UDPSocket
+      .any_instance
+      .should_receive(:send)
+      .with("[Maestro@#{host_ip}:#{port}]", 0, broadcast_ip, port)
   end
 end
 
