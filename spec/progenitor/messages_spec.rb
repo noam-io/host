f0ac2087c1d8309c3878a7a66d9cffb3e4b4a75a
dougbradbury <doug@8thlight.com>
2012-10-17 14:10:36 -0500

    adds some message test for parsing / sending array values

--- spec/progenitor/messages_spec.rb
+++ spec/progenitor/messages_spec.rb
@@ -11,6 +11,15 @@ describe Progenitor::Messages do
     parsed.event_value.should == 65.32
   end
 
+  it "should parse an event message with array values" do
+    message = [ "event", "spalla_id2", "car_speed", ["value1", "value2"] ].to_json
+    parsed = described_class.parse(message)
+    parsed.message_type.should == "event"
+    parsed.spalla_id.should == "spalla_id2"
+    parsed.event_name.should == "car_speed"
+    parsed.event_value.should == ["value1", "value2"]
+  end
+
   it "should parse a registration message" do
     message = [ "register", "spalla_id0", 4423, ["car_speed", "rpm", "volume"], ["headlights"] ].to_json
     parsed = described_class.parse(message)
@@ -38,5 +47,10 @@ describe Progenitor::Messages do
   it "should build an event message" do
     message = [ "event", "spalla_id2", "car_speed", 65.32 ].to_json
     described_class.build_event("spalla_id2", "car_speed", 65.32).should == message
+  end
+
+  it "should build an event with an array value" do
+    message = [ "event", "spalla_id2", "car_speed", ["value1", "value2"] ].to_json
+    described_class.build_event("spalla_id2", "car_speed", ["value1", "value2"]).should == message
   end
 end
