26ea9b067e507133837c121ec34f24ebc0765b8b
dougbradbury <progenitor@ProgenitorCluster.local>
2012-11-08 16:26:01 -0600

    startup script from deployed system

--- bin/startup_scripts/mac/startSpallaApp.sh
+++ bin/startup_scripts/mac/startSpallaApp.sh
@@ -4,6 +4,6 @@ mkdir -p /Users/progenitor/SpallaLog
 
 DEVICE_TYPE=mac nohup /Applications/SpallaApp.app/Contents/MacOS/SpallaApp > /Users/progenitor/SpallaLog/out.txt 2> /Users/progenitor/SpallaLog/error.txt < /dev/null &
 
-echo "#!/bin/sh" > /Applications/SpallaApp.app/Contents/MacOS/killSpallaApp.sh
-echo "kill -9 $!" >> /Applications/SpallaApp.app/Contents/MacOS/killSpallaApp.sh
-chmod +x /Applications/SpallaApp.app/Contents/MacOS/killSpallaApp.sh
+echo "#!/bin/sh" > /Users/progenitor/killSpallaApp.sh
+echo "kill -9 $!" >> /Users/progenitor/killSpallaApp.sh
+chmod +x /Users/progenitor/killSpallaApp.sh
