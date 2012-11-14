d86621e2868e8c67b7b489854dc9ce7b4d3a951f
evan <eshapiro@ideo.com>
2012-11-13 22:53:30 -0800

    changed mac startup script to use ~ reference in path to allow for deployment to other macs

--- bin/startup_scripts/mac/startSpallaApp.sh
+++ bin/startup_scripts/mac/startSpallaApp.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-mkdir -p /Users/progenitor/SpallaLog
+mkdir -p ~/SpallaLog
 
-DEVICE_TYPE=mac nohup /Applications/SpallaApp.app/Contents/MacOS/SpallaApp > /Users/progenitor/SpallaLog/out.txt 2> /Users/progenitor/SpallaLog/error.txt < /dev/null &
+DEVICE_TYPE=mac nohup /Applications/SpallaApp.app/Contents/MacOS/SpallaApp > ~/SpallaLog/out.txt 2> ~/SpallaLog/error.txt < /dev/null &
 
-echo "#!/bin/sh" > /Users/progenitor/killSpallaApp.sh
-echo "kill -9 $!" >> /Users/progenitor/killSpallaApp.sh
-chmod +x /Users/progenitor/killSpallaApp.sh
+echo "#!/bin/sh" > ~/killSpallaApp.sh
+echo "kill -9 $!" >> ~/killSpallaApp.sh
+chmod +x ~/killSpallaApp.sh
