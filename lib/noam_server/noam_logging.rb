b1c27586b9b4df8f025f36de2a14f26776669417
Colin Jones <trptcolin@gmail.com>
2014-02-04 14:50:12 -0600

    Convert tabs to 2 spaces

--- lib/noam_server/noam_logging.rb
+++ lib/noam_server/noam_logging.rb
@@ -14,73 +14,73 @@ require 'thread'
 require 'logging'
 
 module NoamServer
-	class NoamLogging
+  class NoamLogging
 
-		# Exposed to facilitate testing
-		@@loggerClass = Logging.logger
+    # Exposed to facilitate testing
+    @@loggerClass = Logging.logger
 
-		@@MaxPerIteration = 10
-		@@SecondsBetweenIteration = 0.1
+    @@MaxPerIteration = 10
+    @@SecondsBetweenIteration = 0.1
 
     @@shutdown = false
     @@queue = Queue.new
 
-		def self.start_up()
-			@@loggerClass.root.level = CONFIG[:logging][:level]
-			@@loggerClass.root.appenders = CONFIG[:logging][:appenders]
+    def self.start_up()
+      @@loggerClass.root.level = CONFIG[:logging][:level]
+      @@loggerClass.root.appenders = CONFIG[:logging][:appenders]
 
-			@@shutdown = false
+      @@shutdown = false
 
-			@@thread = Thread.new do
-				process_queue
-			end
-		end
+      @@thread = Thread.new do
+        process_queue
+      end
+    end
 
-		def self.process_queue()
-			while true
-				val = @@queue.pop
-				if val == :stop
-					break
-				end
-				@@loggerClass[val[0]].add(val[1], val[2])
-			end
-		end
+    def self.process_queue()
+      while true
+        val = @@queue.pop
+        if val == :stop
+          break
+        end
+        @@loggerClass[val[0]].add(val[1], val[2])
+      end
+    end
 
-		def self.shutdown()
-			if @@shutdown
-				return
-			end
-			@@shutdown = true
-			@@queue << :stop
-			@@thread.join
-		end
+    def self.shutdown()
+      if @@shutdown
+        return
+      end
+      @@shutdown = true
+      @@queue << :stop
+      @@thread.join
+    end
 
-		def self.add(obj, severity, msg = nil)
-			if @@shutdown
-				return
-			end
-			@@queue << [obj, severity, msg]
-		end
+    def self.add(obj, severity, msg = nil)
+      if @@shutdown
+        return
+      end
+      @@queue << [obj, severity, msg]
+    end
 
-		def self.debug(obj, msg=nil)
-			add(obj, ::Logging::LEVELS["debug"], msg)
-		end
+    def self.debug(obj, msg=nil)
+      add(obj, ::Logging::LEVELS["debug"], msg)
+    end
 
-		def self.info(obj, msg=nil)
-			add(obj, ::Logging::LEVELS["info"], msg)
-		end
+    def self.info(obj, msg=nil)
+      add(obj, ::Logging::LEVELS["info"], msg)
+    end
 
-		def self.warn(obj, msg=nil)
-			add(obj, ::Logging::LEVELS["warn"], msg)
-		end
+    def self.warn(obj, msg=nil)
+      add(obj, ::Logging::LEVELS["warn"], msg)
+    end
 
-		def self.error(obj, msg=nil)
-			add(obj, ::Logging::LEVELS["error"], msg)
-		end
+    def self.error(obj, msg=nil)
+      add(obj, ::Logging::LEVELS["error"], msg)
+    end
 
-		def self.fatal(obj, msg=nil)
-			add(obj, ::Logging::LEVELS["fatal"], msg)
-			shutdown
-		end
-	end
+    def self.fatal(obj, msg=nil)
+      add(obj, ::Logging::LEVELS["fatal"], msg)
+      shutdown
+    end
+  end
 end
