b448388e0bbd15ffe4ded94ae843a816dc9c2363
David Brody <dbrody@ideo.com>
2014-02-27 16:02:31 -0800

    Updates UI to handle when server goes down.

--- web/javascripts/views/indexBootstrap/indexBootstrapMain.js
+++ web/javascripts/views/indexBootstrap/indexBootstrapMain.js
@@ -3,6 +3,7 @@ $(function() {
   var players = {};
   var channels = {};
   window.numberOfPlayers = 0;
+  window.serverUp = false;
 
   var processManualEventSubmit = function() {
     $.post( this.action, $(this).serialize() );
@@ -25,11 +26,13 @@ $(function() {
   });
 
   var updateAll = function(){
-    for(player_name in players){
-      players[player_name].draw();
-    }
-    for(channel_name in channels){
-      channels[channel_name].draw(players);
+    if(window.serverUp){
+      for(player_name in players){
+        players[player_name].draw();
+      }
+      for(channel_name in channels){
+        channels[channel_name].draw(players);
+      }
     }
   }
 
@@ -41,6 +44,7 @@ $(function() {
     asyncRefreshRoute: '/arefresh',
     errorMessage: 'Contacting Maestro &hellip;',
     cb: function(results){
+      window.serverUp = true;
       // console.log('cursize',window.numberOfPlayers)
       // console.log('newsize',_.size(results['players']));
 
@@ -97,6 +101,13 @@ $(function() {
       detailViewManager.refresh();
     },
     errorcb: function(error){
+      window.serverUp = false;
+      for(channelName in channels){
+        channels[channelName].remove();
+      }
+      for(playerName in players){
+        players[playerName].remove();
+      }
       $("#serverDownError").fadeIn(500);
     }
   };
