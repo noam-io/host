f29d341a36b4c2b4be02a9360ad33db67bc62045
Eric Meyer <emeyer@8thlight.com>
2014-04-15 10:32:58 -0500

    Fix issue with terminating web sockets
      https://www.pivotaltracker.com/story/show/66571410
    
      Instead of using an internal flag to keep track of connection state,
      we now query into Event Machine to determine the state.
    
      Steps to reproduce bug
        1. Connect to Noam server via web socket (e.g. iOS)
        2. Close web socket connection from client
            At this point, the web socket connection is closed,
            but the TCP socket is still open
            The unbind/onclose callback has not been triggered
        3. Wait for a heartbeat timeout
            This calls terminate, which attempts to close the connection
        4. Closing an already closed web socket raises an error

--- lib/noam_server/web_socket_ear.rb
+++ lib/noam_server/web_socket_ear.rb
@@ -1,32 +1,34 @@
-
 require 'noam/messages'
+require 'em/pure_ruby'
 
 module NoamServer
   class WebSocketEar
+
     def initialize(web_socket)
-      @web_socket = web_socket
-      @terminated = false
+      self.web_socket = web_socket
     end
 
     def send_data(data)
       if active?
-        @web_socket.send("%06d" % data.bytesize)
-        @web_socket.send(data)
+        web_socket.send("%06d" % data.bytesize)
+        web_socket.send(data)
       end
     end
 
     def active?
-      not @terminated
+      !EventMachine::Reactor.instance.get_selectable(web_socket.signature).nil?
     end
 
     def terminate
-      if active?
-        @terminated = true
-        @web_socket.close_websocket
-      end
+      web_socket.close_websocket if active?
     end
 
     def new_connection
     end
+
+    private
+
+    attr_accessor :web_socket
+
   end
 end
--- spec/noam_server/web_socket_ear_spec.rb
+++ spec/noam_server/web_socket_ear_spec.rb
@@ -1,27 +1,64 @@
 require 'noam_server/web_socket_ear'
+require 'mocks/reactor'
 
 def wire_message(expected_message)
   "%06d" % expected_message.size + expected_message
 end
 
 describe NoamServer::WebSocketEar do
-  let( :ws ) { double("web socket") }
-  let( :ear ){ described_class.new( ws ) }
 
-  it "should hear" do
-    message = ""
-    ws.stub(:send) do | data |
-      message << data
+  before(:each) do
+    @web_socket = double("EM Web Socket", :signature => 1337)
+    @ear = NoamServer::WebSocketEar.new(@web_socket)
+    @reactor = Mocks::Reactor.new
+    EventMachine::Reactor.stub(:instance).and_return(@reactor)
+  end
+
+  describe "#send_data" do
+    it "sends data with an active connection" do
+      @reactor.connect_to(@web_socket)
+      message = ""
+      @web_socket.stub(:send) do |data|
+        message << data
+      end
+
+      @ear.send_data('test message')
+
+      message.should == wire_message('test message')
     end
 
-    ear.send_data('test message')
-
-    message.should == wire_message('test message')
+    it "does not send data with no active connection" do
+      @web_socket.should_not_receive(:send)
+      @ear.send_data('test message')
+    end
   end
 
-  it "should terminate" do
-    ws.should_receive(:close_websocket)
-    ear.terminate
+  describe "#active?" do
+    it "is not active if the reactor does not know about the web socket" do
+      @ear.should_not be_active
+    end
+
+    it "is active if the reactor is keeping track of the web socket" do
+      @reactor.connect_to(@web_socket)
+      @ear.should be_active
+    end
   end
+
+  describe "#terminate" do
+    it "closes the web socket with an active connection" do
+      @reactor.connect_to(@web_socket)
+      @web_socket.should_receive(:close_websocket)
+      @ear.terminate
+    end
+
+    it "does not close an already closed connection" do
+      @web_socket.should_not_receive(:close_websocket)
+      @ear.terminate
+    end
+  end
+
+  it "respondes to new_connection" do
+    @ear.should respond_to(:new_connection)
+  end
+
 end
-
