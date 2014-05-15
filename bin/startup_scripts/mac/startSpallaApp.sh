0107f5e8e09adb82243bce4cbae675a2e8a938c9
evan <eshapiro@ideo.com>
2014-05-15 16:22:51 -0700

    removed old spalla startup scripts

--- bin/startup_scripts/mac/startSpallaApp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mkdir -p ~/SpallaLog
-
-DEVICE_TYPE=mac nohup /Applications/SpallaApp.app/Contents/MacOS/SpallaApp > ~/SpallaLog/out.txt 2> ~/SpallaLog/error.txt < /dev/null &
-
-echo "#!/bin/sh" > ~/killSpallaApp.sh
-echo "kill -9 $!" >> ~/killSpallaApp.sh
-chmod +x ~/killSpallaApp.sh
