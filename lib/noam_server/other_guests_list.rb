bb5c880e8ecdcfd54774752055f41caba75236ef
dougbradbury <doug@8thlight.com>
2014-05-20 16:17:59 -0500

    handle other servers dropping off the face of the earth

--- lib/noam_server/other_guests_list.rb
+++ lib/noam_server/other_guests_list.rb
@@ -14,6 +14,21 @@ module NoamServer
       @server_list.on_change { servers_changed }
     end
 
+    def servers_changed
+      clean_servers_list
+      request_new_lemma_lists
+    end
+
+    def response_handler(server, list)
+      guests = list["guests-owned"]
+      NoamLogging.debug(self, "Other Guest Response: #{guests.inspect}")
+      @guests[server["name"]] =  guests
+    end
+
+    def get_all
+      @guests.values.reduce(&:merge!) || {}
+    end
+
     def request_lemmas(server)
       http = EventMachine::Protocols::HttpClient.request(
         :host => server[:ip],
@@ -26,8 +41,18 @@ module NoamServer
       }
     end
 
-    def servers_changed()
-      @server_list.get_all.each do |name, server|
+    def clean_servers_list
+      servers = @server_list.get_all
+      @guests.keys.each do |server|
+        if !servers.include?(server)
+          @guests.delete(server)
+        end
+      end
+    end
+
+    def request_new_lemma_lists
+      servers = @server_list.get_all
+      servers.each do |name, server|
         if name != NoamServer.room_name
           NoamLogging.info(self, "#{name} #{NoamServer.room_name}")
           request_lemmas(server)
@@ -35,15 +60,6 @@ module NoamServer
       end
     end
 
-    def response_handler(server, list)
-      guests = list["guests-owned"]
-      NoamLogging.debug(self, "Other Guest Response: #{guests.inspect}")
-      @guests[server["name"]] =  guests
-    end
-
-    def get_all
-      @guests.values.reduce(&:merge!)
-    end
 
   end
 end
--- spec/noam_server/other_guests_list_spec.rb
+++ spec/noam_server/other_guests_list_spec.rb
@@ -26,17 +26,31 @@ describe NoamServer::OtherGuestsList do
     server_repo.stub(:on_change)
     EventMachine::Protocols::HttpClient.stub(:request).and_return(double(:http, :callback => nil))
     list = described_class.new(server_repo)
-    server_repo.should_receive(:get_all).and_return({"server1" => {}, "server2" => {}})
+    server_repo.should_receive(:get_all).at_least(1).and_return({"server1" => {}, "server2" => {}})
     list.servers_changed
   end
 
   it "drops a lemma that is no longer on a list" do
-
     list = described_class.new(server_repo)
     list.response_handler( {"name" => "server1"}, { "guests-owned" => {"Lemma1"=>{"name"=>"Lemma1"}}})
     list.get_all.size.should == 1
     list.response_handler( {"name" => "server1"}, { "guests-owned" => {"Lemma4"=>{"name"=>"Lemma4"}}})
     list.get_all.should_not have_key("Lemma1")
     list.get_all.size.should == 1
+  end
+
+  it "drops a server's lemmas when the server no longer exists" do
+    list = described_class.new(server_repo)
+    list.response_handler( {"name" => "server1"}, { "guests-owned" => {"Lemma1"=>{"name"=>"Lemma1"}}})
+    list.response_handler( {"name" => "server2"}, { "guests-owned" => {"Lemma4"=>{"name"=>"Lemma4"}}})
+
+    server_repo.stub(:on_change)
+    EventMachine::Protocols::HttpClient.stub(:request).and_return(double(:http, :callback => nil))
+    server_repo.stub(:get_all).and_return({"server2" => {}})
+    list.servers_changed
+
+    list.get_all.size.should == 1
+    list.get_all.should_not have_key("Lemma1")
+    list.get_all.should have_key("Lemma4")
   end
 end
